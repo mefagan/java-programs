56292558f4a844964d4c59720cf35a18e9db2648
MaryEileen Fagan <maryeileen.fagan@gmail.com>
2016-07-07 10:36:18 -0400

    added test classes for addressbook and contact

---
+++ PS3/src/addressbook/AddressBookTest.java
@@ -0,0 +1,104 @@
+package addressbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddressBookTest {
+
+	@Test
+	public final void testAddressBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testAddressBookString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testAddContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testRemoveContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testToString1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ PS3/src/addressbook/ContactTest.java
@@ -0,0 +1,104 @@
+package addressbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactTest {
+
+	@Test
+	public final void testMatch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetEmailAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetNote() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testToString1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
